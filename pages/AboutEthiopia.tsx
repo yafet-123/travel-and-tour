1cda0e3d0d04ff2834637975633a0602dee2d6ee
yafet-123 <yafetaddisu123@gmail.com>
2022-12-28 16:02:30 -0800

    feature:adding about ethopia

--- pages/AboutEthiopia.tsx
+++ pages/AboutEthiopia.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from '../components/Navbar';
 import { AboutEthiopiaHero } from '../components/AboutEthiopiaHero';
-import { AboutEthiopiaDetail } from '../components/AboutEthiopiaDetail'
+import { AboutEthiopiaDetail } from '../components/AboutEthiopiaDetail';
 import { Footer } from '../components/Footer';
 
 export default function AboutEthiopia() {
