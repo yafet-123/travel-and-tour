94e2c17cd45a93114585cba2a8abc739d49a6be6
yafet-123 <yafetaddisu123@gmail.com>
2023-01-04 11:24:26 -0800

    adding the hero page

--- components/Herotours.tsx
+++ components/Herotours.tsx
@@ -1,47 +1,49 @@
-import Image from 'next/image';
-import heroImageOne from '../public/hero/heroImageOne.jpg';
-import heroImageTwo from '../public/hero/heroImageTwo.jpg';
-import heroImageThree from '../public/hero/heroImageThree.jpg';
+import React from 'react';
+import Link from 'next/link';
+
 export const Herotours: React.FC = () => {
-  const TourArray = [
+  const CardsArray = [
     {
-      image: heroImageOne,
+      id: 'historical',
       name: 'Historical Routes',
-      description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing',
     },
     {
-      image: heroImageTwo,
+      id: 'trekking',
       name: 'Mountain Trekking',
-      description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing',
     },
     {
-      image: heroImageThree,
+      id: 'adventure',
       name: 'Adventure Expeditions',
-      description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+      description: 'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing',
     },
   ];
 
   return (
-    <div className="flex flex-col text-white my-12 mx-[5%] justify-between lg:flex-row lg:-mt-20 lg:my-10">
-      {TourArray.map((data, index) => (
-        <div
-          key={index}
-          className="w-full lg:w-[30%] h-[670px] bg-[#94BB3C] mb-10 p-2 relative"
-        >
-          <Image
-            src={data.image}
-            className="w-full h-[650px] brightness-50 object-cover box-border"
-            priority
-            alt="Navbar"
-          />
-          <h3 className="absolute top-10 px-10 w-full lg:w-10 text-4xl text-center lg:text-left font-sans leading-10 font-bold text-white hover:text-[#94BB3C]">
-            {data.name}
-          </h3>
-          <p className="mb-10 absolute bottom-5 px-10 w-full lg:w-72 text-2xl text-center lg:text-left font-sans leading-10 font-bold text-white hover:text-[#94BB3C]">
-            {data.description}
-          </p>
-        </div>
-      ))}
+    <div className="flex flex-col text-white my-12 w-11/12 mx-auto justify-center space-y-4 md:space-y-0 md:space-x-8 md:flex-row space-x-0 md:-mt-12 md:h-128 md:my-24">
+      {CardsArray.map((card, index) => {
+        return (
+          <div
+            key={index}
+            className={`${card.id} bg-cover bg-blend-darken justify-between 
+                        sm:h-96 flex flex-col place-items-center text-center 
+                        border-secondaryColor border-4 md:h-96 py-3`}
+          > 
+          <div> <h3 className="text-3xl font-bold font-monospace ">{card.name}</h3></div>
+         
+          <div className='flex flex-col w-3/4 items-center gap-4 '>
+            <p className="text-md  text-white">{card.description}</p>
+            <Link href="/">
+              <h5 className="border text-white font-semibold font-monospace hover:font-bold mt-2 p-1 ">
+                Read More
+              </h5>
+            </Link>
+          </div>
+            
+          </div>
+        );
+      })}
     </div>
   );
 };
