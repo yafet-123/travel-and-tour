54c669ba054987758a47f533a231f0d9e6ab4d58
yafet-123 <yafetaddisu123@gmail.com>
2023-01-08 08:46:20 -0800

    editing the sustenability

--- components/DestinationsDetail.tsx
+++ components/DestinationsDetail.tsx
@@ -4,15 +4,15 @@ import tana from '../public/lake-Tana.png';
 import fasiledes from '../public/fasiledes.jpg';
 import lalibela from '../public/lalibela-destination.png';
 import axum from '../public/axum.png';
-import DestinationsImageFive from '../public/2.jpg';
+import SimienMountainsNationalPark from '../public/Simien Mountains National Park.png';
 import BalemountainsNationalPark from '../public/Attractions-naturall-endemic-mammals-MOUNTAIN-NYALA-overland-ethiopia-tour.jpg';
-import danakil from '../public/danakil-hero.jpg';
-import DestinationsImageEight from '../public/2.jpg';
-import DestinationsImageNine from '../public/2.jpg';
-import DestinationsImageTen from '../public/2.jpg';
-import DestinationsImageEleven from '../public/2.jpg';
-import DestinationsImageTwelve from '../public/2.jpg';
-import DestinationsImageThirteen from '../public/2.jpg';
+import DanakilDepression from '../public/Danakil Depression.png';
+import AwashNationalPark from '../public/Awash National Park.png';
+import OmoValley from '../public/OmoValley.png';
+import Harar from '../public/Harar.png';
+import Meskel from '../public/Meskel.png';
+import Timket from '../public/Timket.png';
+import Genna from '../public/Genna.png';
 export const DestinationsDetail: React.FC = ({ state }) => {
   const HistoricalDetail = [
     {
@@ -49,7 +49,7 @@ export const DestinationsDetail: React.FC = ({ state }) => {
       head: 'Simien Mountains National Park',
       description:
         'The Lake Tana area was important in the fourteenth, fifteenth and sixteenth centuries in view of its role in maintaining the Christian faith against contemporary pressures, and the rise of the Solomonic Dynasty which patronized the building of churches and monasteries. <br className="pb-10"> Many of the earliest manuscripts and precious examples of ecclesiastical art as well as royal objects were safely stored in their treasuries. At the same time, new religious arts were developed and displayed in the churches. The oldest of these forty or so churches have their origins in the fourteenth century. They reflect the native building tradition in their round shape, materials and building techniques. Each one consists of three parts: the inner sanctuary, the inner ambulatory and the outer ambulatory. The outside walls of the sanctuary are usually covered with magnificent paintings. Seven of the most accessible and representative of these churches, still serving their original function, have been selected for the international campaign: Kebran Gabriel, Ura Kidane Mehret, Narga Selassie, Daga Estifanos, Medhane Alem of Rema, Kota Maryam and Mertola Maryam.',
-      image: DestinationsImageFive,
+      image: SimienMountainsNationalPark,
     },
 
     {
@@ -63,14 +63,14 @@ export const DestinationsDetail: React.FC = ({ state }) => {
       head: 'Danakil Depression',
       description:
         'The Lake Tana area was important in the fourteenth, fifteenth and sixteenth centuries in view of its role in maintaining the Christian faith against contemporary pressures, and the rise of the Solomonic Dynasty which patronized the building of churches and monasteries. <br className="pb-10"/> Many of the earliest manuscripts and precious examples of ecclesiastical art as well as royal objects were safely stored in their treasuries. At the same time, new religious arts were developed and displayed in the churches. The oldest of these forty or so churches have their origins in the fourteenth century. They reflect the native building tradition in their round shape, materials and building techniques. Each one consists of three parts: the inner sanctuary, the inner ambulatory and the outer ambulatory. The outside walls of the sanctuary are usually covered with magnificent paintings. Seven of the most accessible and representative of these churches, still serving their original function, have been selected for the international campaign: Kebran Gabriel, Ura Kidane Mehret, Narga Selassie, Daga Estifanos, Medhane Alem of Rema, Kota Maryam and Mertola Maryam.',
-      image: danakil,
+      image: DanakilDepression,
     },
 
     {
       head: 'Awash National Park',
       description:
         'It stands 32m tall and has a crenulated parapet and four domed towers. Made of roughly hewn stones, it’s reputedly the work of an Indian architect and shows an unusual synthesis of Indian, Portuguese, Moorish and Aksumite influences. The main floor was used as dining halls and a formal reception area. <br className="pb-10" /> Note the wall reliefs, including several Stars of David, which trumpet Fasiladas’ link to the Solomonic dynasty. The small room in the northern corner boasts its original beam ceiling and some faint frescoes. On the 1st floor, Fasiladas’ prayer room has windows in four directions, each overlooking Gonder’s important churches. Religious ceremonies were held on the roof, and it was from here that he addressed his people. Above Fasiladas’ 2nd-floor bedroomwas the watchtower, from where it’s (apparently) possible to see all the way to Lake Tana. Behind the castle are various ruined buildings, including the kitchen (domed ceiling), steam bath and water cistern.',
-      image: DestinationsImageEight,
+      image: AwashNationalPark,
     },
   ];
 
@@ -79,14 +79,14 @@ export const DestinationsDetail: React.FC = ({ state }) => {
       head: 'Omo Valley',
       description:
         'The Lake Tana area was important in the fourteenth, fifteenth and sixteenth centuries in view of its role in maintaining the Christian faith against contemporary pressures, and the rise of the Solomonic Dynasty which patronized the building of churches and monasteries. <br className="pb-10" /> Many of the earliest manuscripts and precious examples of ecclesiastical art as well as royal objects were safely stored in their treasuries. At the same time, new religious arts were developed and displayed in the churches. The oldest of these forty or so churches have their origins in the fourteenth century. They reflect the native building tradition in their round shape, materials and building techniques. Each one consists of three parts: the inner sanctuary, the inner ambulatory and the outer ambulatory. The outside walls of the sanctuary are usually covered with magnificent paintings. Seven of the most accessible and representative of these churches, still serving their original function, have been selected for the international campaign: Kebran Gabriel, Ura Kidane Mehret, Narga Selassie, Daga Estifanos, Medhane Alem of Rema, Kota Maryam and Mertola Maryam.',
-      image: DestinationsImageNine,
+      image: OmoValley,
     },
 
     {
       head: 'Harar',
       description:
         'It stands 32m tall and has a crenulated parapet and four domed towers. Made of roughly hewn stones, it’s reputedly the work of an Indian architect and shows an unusual synthesis of Indian, Portuguese, Moorish and Aksumite influences. The main floor was used as dining halls and a formal reception area. <br className="pb-10" /> Note the wall reliefs, including several Stars of David, which trumpet Fasiladas’ link to the Solomonic dynasty. The small room in the northern corner boasts its original beam ceiling and some faint frescoes. On the 1st floor, Fasiladas’ prayer room has windows in four directions, each overlooking Gonder’s important churches. Religious ceremonies were held on the roof, and it was from here that he addressed his people. Above Fasiladas’ 2nd-floor bedroomwas the watchtower, from where it’s (apparently) possible to see all the way to Lake Tana. Behind the castle are various ruined buildings, including the kitchen (domed ceiling), steam bath and water cistern.',
-      image: DestinationsImageTen,
+      image: Harar,
     },
   ];
 
@@ -95,21 +95,21 @@ export const DestinationsDetail: React.FC = ({ state }) => {
       head: 'Meskel (Finding of the true cross)',
       description:
         'The Lake Tana area was important in the fourteenth, fifteenth and sixteenth centuries in view of its role in maintaining the Christian faith against contemporary pressures, and the rise of the Solomonic Dynasty which patronized the building of churches and monasteries. <br className="pb-10" /> Many of the earliest manuscripts and precious examples of ecclesiastical art as well as royal objects were safely stored in their treasuries. At the same time, new religious arts were developed and displayed in the churches. The oldest of these forty or so churches have their origins in the fourteenth century. They reflect the native building tradition in their round shape, materials and building techniques. Each one consists of three parts: the inner sanctuary, the inner ambulatory and the outer ambulatory. The outside walls of the sanctuary are usually covered with magnificent paintings. Seven of the most accessible and representative of these churches, still serving their original function, have been selected for the international campaign: Kebran Gabriel, Ura Kidane Mehret, Narga Selassie, Daga Estifanos, Medhane Alem of Rema, Kota Maryam and Mertola Maryam.',
-      image: DestinationsImageEleven,
+      image: Meskel,
     },
 
     {
       head: 'Timket',
       description:
         'It stands 32m tall and has a crenulated parapet and four domed towers. Made of roughly hewn stones, it’s reputedly the work of an Indian architect and shows an unusual synthesis of Indian, Portuguese, Moorish and Aksumite influences. The main floor was used as dining halls and a formal reception area. <br className="pb-20" /> Note the wall reliefs, including several Stars of David, which trumpet Fasiladas’ link to the Solomonic dynasty. The small room in the northern corner boasts its original beam ceiling and some faint frescoes. On the 1st floor, Fasiladas’ prayer room has windows in four directions, each overlooking Gonder’s important churches. Religious ceremonies were held on the roof, and it was from here that he addressed his people. Above Fasiladas’ 2nd-floor bedroomwas the watchtower, from where it’s (apparently) possible to see all the way to Lake Tana. Behind the castle are various ruined buildings, including the kitchen (domed ceiling), steam bath and water cistern.',
-      image: DestinationsImageTwelve,
+      image: Timket,
     },
 
     {
       head: 'Genna (Ethiopian Christmass)',
       description:
         'The Lake Tana area was important in the fourteenth, fifteenth and sixteenth centuries in view of its role in maintaining the Christian faith against contemporary pressures, and the rise of the Solomonic Dynasty which patronized the building of churches and monasteries. <br className="pb-10" /> Many of the earliest manuscripts and precious examples of ecclesiastical art as well as royal objects were safely stored in their treasuries. At the same time, new religious arts were developed and displayed in the churches. The oldest of these forty or so churches have their origins in the fourteenth century. They reflect the native building tradition in their round shape, materials and building techniques. Each one consists of three parts: the inner sanctuary, the inner ambulatory and the outer ambulatory. The outside walls of the sanctuary are usually covered with magnificent paintings. Seven of the most accessible and representative of these churches, still serving their original function, have been selected for the international campaign: Kebran Gabriel, Ura Kidane Mehret, Narga Selassie, Daga Estifanos, Medhane Alem of Rema, Kota Maryam and Mertola Maryam.',
-      image: DestinationsImageThirteen,
+      image: Genna,
     },
   ];
 
@@ -121,9 +121,9 @@ export const DestinationsDetail: React.FC = ({ state }) => {
             <div key={index} className="p-3 lg:p-20 ">
               <Image
                 src={data.image}
-                className="w-full lg:w-[782px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-10 float-none lg:odd:float-right lg:even:float-left"
+                className="w-full lg:w-[700px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-5 float-none lg:odd:float-right lg:even:float-left mb-10"
                 priority
-                alt="AboutUsEthiopia"
+                alt="Destination"
               />
 
               <div>
@@ -132,7 +132,7 @@ export const DestinationsDetail: React.FC = ({ state }) => {
                 </h1>
                 <div
                   dangerouslySetInnerHTML={{ __html: data.description }}
-                  className="text-left text-lg lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
+                  className="text-left text-md lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
                 />
               </div>
             </div>
@@ -146,9 +146,9 @@ export const DestinationsDetail: React.FC = ({ state }) => {
             <div key={index} className="p-3 lg:p-20">
               <Image
                 src={data.image}
-                className="w-full lg:w-[782px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-10 odd:float-right even:float-left"
+                className="w-full lg:w-[700px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-5 odd:float-right even:float-left mb-10"
                 priority
-                alt="AboutUsEthiopia"
+                alt="Destination"
               />
 
               <div>
@@ -157,7 +157,7 @@ export const DestinationsDetail: React.FC = ({ state }) => {
                 </h1>
                 <div
                   dangerouslySetInnerHTML={{ __html: data.description }}
-                  className="text-left text-lg lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
+                  className="text-left text-md lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
                 />
               </div>
             </div>
@@ -171,9 +171,9 @@ export const DestinationsDetail: React.FC = ({ state }) => {
             <div key={index} className="p-3 lg:p-20">
               <Image
                 src={data.image}
-                className="w-full lg:w-[782px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-10 odd:float-right even:float-left"
+                className="w-full lg:w-[700px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-5 odd:float-right even:float-left mb-10"
                 priority
-                alt="AboutUsEthiopia"
+                alt="Destination"
               />
 
               <div>
@@ -182,7 +182,7 @@ export const DestinationsDetail: React.FC = ({ state }) => {
                 </h1>
                 <div
                   dangerouslySetInnerHTML={{ __html: data.description }}
-                  className="text-left text-lg lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
+                  className="text-left text-md lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
                 />
               </div>
             </div>
@@ -196,9 +196,9 @@ export const DestinationsDetail: React.FC = ({ state }) => {
             <div key={index} className="p-3 lg:p-20">
               <Image
                 src={data.image}
-                className="w-full lg:w-[782px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-10 odd:float-right even:float-left"
+                className="w-full lg:w-[700px] h-[300px] object-cover brightness-75 border rounded-2xl lg:mx-5 odd:float-right even:float-left mb-10"
                 priority
-                alt="AboutUsEthiopia"
+                alt="Destination"
               />
 
               <div>
@@ -207,7 +207,7 @@ export const DestinationsDetail: React.FC = ({ state }) => {
                 </h1>
                 <div
                   dangerouslySetInnerHTML={{ __html: data.description }}
-                  className="text-left text-lg lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
+                  className="text-left text-md lg:text-xl font-sans leading-10 font-bold text-[#165248] hover:text-[#94BB3C] mb-5"
                 />
               </div>
             </div>
